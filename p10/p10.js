f70514f9cb748b70f1cb4920358692c69de3bab3
whroman <me@walterroman.com>
2016-10-08 15:12:11 +0200

    adding p10, renaming dirs

---
+++ p10/p10.js
@@ -0,0 +1,8 @@
+function findPrimesBelow(num) {
+    const primesList = [];
+    for (let iter = 1; iter < num; iter++) {
+        if (primesList.includes(iter)) break;
+        if (parseInt(Math.sqrt(iter)))
+    }
+
+}
